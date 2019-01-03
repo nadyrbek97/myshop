e7fed533970b4c29f663abe0887a8ac8e760a317
nadyrbek97 <nadyrbek97@gmail.com>
2019-01-03 16:50:19 +0600

    registering customer orders

--- myshop/urls.py
+++ myshop/urls.py
@@ -22,7 +22,8 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('cart/', include('cart.urls', namespace='cart')),
-    path('', include('shop.urls', namespace='shop'))
+    path('', include('shop.urls', namespace='shop')),
+    path('orders/', include('orders.urls', namespace='orders')),
 
 ]
 if settings.DEBUG:
